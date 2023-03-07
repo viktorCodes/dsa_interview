a09379f5f1065ec3031064c762ee39712209952e
calimendoza <mbaivictor1@gmail.com>
2023-03-06 22:36:35 -0800

    added Iterative and Regex solutions

--- countingVowelsInString/index.js
+++ countingVowelsInString/index.js
@@ -20,3 +20,45 @@ iv)pseudocode: write  a function that counts and returns the number of vowels on
 
 
 */
+
+//Iterative Approach
+//In this approach, we loop through each letter of the string passed and then check to see if they match any of the English vowels. Before running through the text we create a counter initialized to a value of zero. When a match is found, the counter is incremented. At the end of it all, we return the counter which represents the total number of vowels found.
+
+const Vowels = ["a", "e", "i", "e", "o", "u"]
+
+function vowelCounter(text){
+
+    //Initialize counter
+    let count = 0;
+
+    //Loop through text to test if character is a vowel
+    for(let letter of text.toLowerCase()){
+        if(Vowels.includes(letter)){
+            count++
+        }
+    }
+
+    //Return number of vowels
+    return count
+
+}
+
+
+
+
+//Using Regular Expressions
+
+function vowelsCounter(text){
+    //Search text with Regex and store all matching instances
+    let matchingInstances = text.match(/[aeiou]/gi);
+
+    //Check if matching instances exist then calculate
+
+    if(matchingInstances){
+        //Return number of vowels
+
+        return matchingInstances.length
+    } else {
+        return 0
+    }
+}
